449de6b061e63cbfc3de204d43fff944a26bbdda
elisonric <elison.ricardo@gmail.com>
2020-05-14 12:30:21 -0300

    Implementação da fila com vetor

---
+++ src/main/java/FilaVector/FilaVector.java
@@ -0,0 +1,43 @@
+package FilaVector;
+
+public class FilaVector {
+
+    private Object[] vector;
+    private int first;
+    private int last;
+    public int length;
+
+    public FilaVector() {
+        this.vector = new Object[10];
+        this.first = 0;
+        this.last = 0;
+        this.length = 0;
+    }
+
+    public void insert(Object o) {
+        if (isFull()) {
+            throw new RuntimeException("Fila cheia, não há espaço para inserir!");
+        }
+        this.vector[last] = o;
+        last = (last + 1) % vector.length;
+        length++;
+    }
+
+    public Object remove() {
+        if (isEmpty()) {
+            throw new RuntimeException("Fila vazia, nada a ser removido!");
+        }
+        Object element = this.vector[first];
+        first = (first +1) % vector.length;
+        length--;
+        return element;
+    }
+
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public boolean isFull() {
+        return length == vector.length;
+    }
+}
